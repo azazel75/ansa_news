616139cb94e7f048127b0affebc1df4e45089f21
Alberto Berti <alberto@metapensiero.it>
2014-05-25 22:55:59 +0200

    Alloy con listview funzionante

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,11 @@
 var posts = Alloy.Collections.post;
-posts
 
+function preparePostData(pmodel) {
+    var post = pmodel.toJSON();
+    post.searchableText = post.title + ' ' + post.description;
+    console.log(post);
+    return post;
+}
+
+posts.fetchRSS();
 $.index.open();
--- app/models/post.js
+++ app/models/post.js
@@ -1,27 +1,37 @@
 exports.definition = {
-	config: {
-		columns: {
-		    "link": "text",
-		    "title": "text",
-		    "description": "text"
-		},
-		adapter: {
-			type: "sql",
-			collection_name: "post"
-		}
-	},
-	extendModel: function(Model) {
-		_.extend(Model.prototype, {
-			// extended functions and properties go here
-		});
+    config: {
+        columns: {
+            "link": "text",
+            "title": "text",
+            "date": "text",
+            "description": "text"
+        },
+        adapter: {
+            type: "sql",
+            collection_name: "post",
+            idAttribute: 'link'
+        }
+    },
+    extendModel: function(Model) {
+        _.extend(Model.prototype, {
 
-		return Model;
-	},
-	extendCollection: function(Collection) {
-		_.extend(Collection.prototype, {
-			// extended functions and properties go here
-		});
+        });
 
-		return Collection;
-	}
+        return Model;
+    },
+    extendCollection: function(Collection) {
+        _.extend(Collection.prototype, {
+            fetchRSS: function(url) {
+                var rss = require('rss'),
+                    self = this,
+                    data;
+                function handleFeedData(feed) {
+                    self.add(feed.blog_posts);
+                }
+                rss.getRSSData(url, handleFeedData);
+            }
+        });
+
+        return Collection;
+    }
 };
