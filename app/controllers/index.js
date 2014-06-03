ab5a54af10854d420ef5910428d7708a04467476
Alberto Berti <alberto@metapensiero.it>
2014-06-03 09:41:43 +0200

    Aggiunta azione "aggiungi feed" e codice di dettaglio

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,25 @@
 var feeds = Alloy.Collections.feed;
+var utils = require('utils');
 function showPosts(e) {
     var feed = feeds.at(e.itemIndex);
     var posts = Alloy.createController('posts', feed)
         .getView();
     posts.open();
 }
+
+function addFeed(e) {
+    var feed = Alloy.createModel('feed');
+    var feed_detail = Alloy.createController('feed_detail', {feed: feed, callback: afterAddClose})
+        .getView();
+    feed_detail.open();
+}
+
+function afterAddClose(result, feed) {
+    if (result === 'ok') {
+        feed.save();
+        feeds.add(feed);
+    }
+}
+
 feeds.fetch();
 $.index.open();
